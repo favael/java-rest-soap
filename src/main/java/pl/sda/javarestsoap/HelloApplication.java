67e402841ca63582b5c6be19d47f932629bda64c
Rafal Dogoda <rafaldogoda86@gmail.com>
2022-06-23 18:53:20 +0200

    [init] - projekt skeleton created

---
+++ src/main/java/pl/sda/javarestsoap/HelloApplication.java
@@ -0,0 +1,9 @@
+package pl.sda.javarestsoap;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
